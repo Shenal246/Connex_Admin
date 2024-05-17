603cc4e642f5074254fecb9c7e3385ffe2339579
Shenal Perera <pererashenal369@gmail.com>
2024-05-17 14:17:40 +0530

    FrontEnd design Completed

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,13 +1,13 @@
 
 import './App.css';
-// import Login from './Components/Login/Login';
-import NavigationBar from './Components/Navbar/NavigationBar'
+import Login from './Components/Login/Login';
+// import NavigationBar from './Components/Navbar/NavigationBar'
 
 function App() {
   return (
     <div className="App">
-      <NavigationBar />
-      {/* <Login/> */}
+      {/* <NavigationBar /> */}
+      <Login/>
     </div>
   );
 }
